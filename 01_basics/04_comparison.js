9f949253c895e00d23e85db2ff6fa95f1cf32014
saqibansarii <najams146@gmail.com>
2024-10-09 13:18:54 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+//console.log(2>1)
+//console.log(2>=1)
+//console.log(2<1)
+//console.log(2==1)
+//console.log(2!=1)
+
+//console.log("2" > 1)
+
+//console.log(null > 0)
+//console.log(null < 0)
+//console.log(null >= 0)
+
+//console.log(undefined == 0)
+//console.log(undefined > 0)
+//console.log(undefined < 0)
+
+
+//Strict Check is checked by Tripple equal sign ===
+
+console.log("2" === 2)
