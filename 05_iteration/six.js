4bb0554a30844f9c443ea3bef3918035130b0308
saqibansarii <najams146@gmail.com>
2024-10-19 15:48:49 +0000

    filter function

---
+++ 05_iteration/six.js
@@ -0,0 +1,33 @@
+// const coding = ["js", "rubby", "python", "cpp", "java"]
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values)
+
+
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//filter function
+//both methods are correct 
+// const newNums = myNums.filter( (num)=> (num > 4))
+// const newNums = myNums.filter( (num)=> {
+//     return num > 4
+// })
+
+
+
+//same example with for each loop
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums)
+
+
