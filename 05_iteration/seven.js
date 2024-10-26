06865c4da3efdd11b2c2620b32dbadc6933f5308
saqibansarii <najams146@gmail.com>
2024-10-26 06:33:26 +0000

    loop more concept

--- 05_iteration/seven.js
+++ 05_iteration/seven.js
@@ -1,13 +1,24 @@
-const myNumbers = [1,2, 3, 4, 5, 6, 7, 8, 9, 10]
-const newNumbers = []
-myNumbers.forEach( (item) => {
+//same example is given top with foreach loop
+// const myNumbers = [1,2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNumbers = []
+// myNumbers.forEach( (item) => {
 
-    newNumbers.push(item + 10)
-} )
-console.log(newNumbers)
+//     newNumbers.push(item + 10)
+// } )
+// console.log(newNumbers)
 
+
+//map function
 // const myNumbers = [1,2, 3, 4, 5, 6, 7, 8, 9, 10]
 // const newNums = myNumbers.map( (num) => {
 //     return num + 10
 // } )
 // console.log(newNums)
+
+
+const myNumbers = [1,2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNums = myNumbers
+                .map((num) => num * 10)
+                .map((num) => num + 1)
+                .filter( (num) => num >=40 )
+console.log(newNums)
